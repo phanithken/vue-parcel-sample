5fcf7490aecb25d482efb3c2238df3d954697fb4
Ken Phanith <phanith.ken@quad.co.jp>
2019-02-01 11:26:30 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+// template
+<template>
+    <div class="container">
+        <h1>{{ message }}</h1>
+        <p-button @click.native="decrement" :text="'-'"></p-button>
+        <h3>{{ count }}</h3>
+        <p-button @click.native="increment" :text="'+'"><p-button>
+    </div>
+</template>
+
+// script
+<script>
+    import PButton from './Button'
+    export default {
+        name: 'App',
+        components: {
+            PButton
+        },
+        data() {
+            return {
+                message: 'Hello Parcel',
+                count: 0
+            }
+        },
+        methods: {
+            increment () {
+                this.count++
+            },
+
+            decrement () {
+                this.count--
+            }
+        }
+    }
+</script>
+
+// style
+<style scroped lang="scss">
+    $blue-color: blue;
+    .container {
+        width: 600px;
+        margin: 50px auto;
+        text-align: center;
+
+        h1 {
+            color: $blue-color;
+        }
+    }
+</style>
---
+++ src/Button.vue
@@ -0,0 +1,31 @@
+// template
+<template>
+    <button>{{ text }}</button>
+</template>
+
+// script
+<script>
+    export default {
+        name: 'p-button',
+        props: {
+            text: { required: true }
+        },
+        data() {
+            return {
+                message: ""
+            }
+        }
+    }
+</script>
+
+// style
+<style scoped lang="scss">
+    button {
+        height: 50px;
+        width: 50px;
+        color: white;
+        border: none;
+        background: black;
+        border-radius: 5px;
+    }
+</style>
